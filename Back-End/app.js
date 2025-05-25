6fa4aa0fa9ec5718d1554ccb9b59ae416d42a3a6
Jeniel Pusa <jeniel12300@gmail.com>
2025-05-26 01:37:53 +0800

    Error for deployment238

--- Back-End/app.js
+++ Back-End/app.js
@@ -40,7 +40,6 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      //mongoUrl: 'mongodb+srv://admin:FV0X2MY5DafZ4kUJ@cluster0.qpiekbv.mongodb.net/ThesisDatabase?retryWrites=true&w=majority&appName=Cluster0',
       mongoUrl: process.env.CONN_STR,
       ttl: 24 * 60 * 60, // 24 hours in seconds
     }),
