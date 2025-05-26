f1cf1889d845a75b636d0a5104e62621a3c21196
Jeniel Pusa <jeniel12300@gmail.com>
2025-05-26 12:24:05 +0800

    Fix about Back-End2

--- Back-End/app.js
+++ Back-End/app.js
@@ -53,7 +53,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://web-based-newborn-tracking-system.onrender.com",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
